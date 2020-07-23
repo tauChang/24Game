b863550737dd0b5c254c0d03548279429dee08c3
tauChang <44998547+tauChang@users.noreply.github.com>
2020-07-23 22:09:04 +0800

    Update main.py
    Deal with winner(s). Beautify output. Handle exceptions.

--- main.py
+++ main.py
@@ -26,30 +26,52 @@ class Game:
     def printScore(self):
         for player in self.players:
             print("> ", player, ":", self.score[player])
+        print("\n")
 
     def roundWinner(self):
         winner = input("Who's the winner? ")
         while winner not in self.players:
-            winner = input("Type again, dumdum. ")
+            winner = input("No such player. Type again, dumdum. ")
         print("Good job, %s!" % winner)
         self.score[winner] += 1
 
     def finalWinner(self):
-        return []
+        return [p for p in self.score if self.score[p] == max(self.score.values())]
         # returns a list
 
     def farewell(self):
         print("The game has ended! The score is\n")
         self.printScore()
-        print("\n%s is the winner! Congratulations!" % 'a')
+        winners = self.finalWinner()
+        if len(winners) > 1:
+            print(*winners[:-1], "and %s are the winners! Congratulations!" % winners[-1], sep = ", ")
+        else:
+            print("\n%s is the winner! Congratulations!" % 'a')
         
 
     def start(self):
         print("Welcome to The 24 Game!\n")
-        self.roundCount = int(input("How many rounds do you want to play? "))
-        self.playerCount = int(input("How many players are there? "))
+        while True:
+            try:
+                self.roundCount = int(input("How many rounds do you want to play? "))
+                break
+            except ValueError:
+                print("You are giving the wrong input. Try again, dumdum\n")
+        while True:
+            try:
+                self.playerCount = int(input("How many players are there? "))
+                break
+            except ValueError:
+                print("You are giving the wrong input. Try again, dumdum\n")
+        
+            
         for i in range(self.playerCount):
-            name = input("Player %d's name: " % (i+1))
+            while True:
+                name = input("Player %d's name: " % (i+1))
+                if name in self.players:
+                    print("This name is already taken. Give this player another name.")
+                else:
+                    break
             self.players.append(name)
             self.score[name] = 0
         print("Great! Let's start the game!")
@@ -57,9 +79,10 @@ class Game:
         r = 1
         while r <= self.roundCount:
             print("\n==============================")
+            print("~ Round %d ~\n" %r)
             self.printScore()
             cards = self.giveCard()
-            print(cards)
+            print(cards, "\n")
             self.roundWinner()
             # Deal with winner
             print("==============================\n")
